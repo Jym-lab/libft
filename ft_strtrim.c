b1d443cee9c2fb010a3b0ec01a9c3e50a29c101b
Youngmin Joo <yjoo@c9r1s7.42seoul.kr>
2021-11-17 18:25:01 +0900

    new

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,5 +13,19 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	size_t	start;
+	size_t	end;
+	char	*trimstr;
 
+	start = 0;
+	end = ft_strlen(s1);
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (s1[end - 1] && ft_strchr(set, s1[end - 1]))
+		end--;
+	trimstr = (char *)malloc(sizeof(char) * end - start + 1);
+	if (!trimstr)
+		return (NULL);
+	ft_strlcat(trimstr, s1 + start, end - start + 1);
+	return (trimstr);
 }
