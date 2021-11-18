edfae7cba5c96de1c82d9a7c09b19475edb4a89f
Jym-lab <jym9809@gmail.com>
2021-11-19 05:35:22 +0900

    1차 테스트

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: yjoo <yjoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 18:29:18 by yjoo              #+#    #+#             */
-/*   Updated: 2021/11/17 21:34:26 by yjoo             ###   ########.fr       */
+/*   Updated: 2021/11/18 17:15:51 by yjoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char	*f_str;
-	size_t	s_len;
 	size_t	i;
 
-	i = 0;
-	s_len = ft_strlen(s);
-	f_str = (char *)malloc(sizeof(char) * (s_len + 1));
-	if (!f_str)
+	if (!s || !f)
 		return (NULL);
-	while (i < s_len)
+	i = 0;
+	f_str = ft_strdup(s);
+	while (f_str[i])
 	{
 		f_str[i] = f(i, s[i]);
 		i++;
 	}
-	f_str[i] = 0;
 	return (f_str);
 }
--- libft.h
+++ libft.h
@@ -17,6 +17,16 @@
 # include <stdlib.h>
 # include <limits.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
 int		ft_isalpha(int c);
 int		ft_islower(int c);
 int		ft_isupper(int c);
@@ -29,6 +39,7 @@ int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_atoi(const char *nptr);
+int		ft_lstsize(t_list *lst);
 
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
@@ -51,5 +62,15 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
